08b56a14892077b3c11cff1b6c7f3c88fc46c2f4
Jason Cassella <jasoncassella1@gmail.com>
2023-06-29 10:50:43 -0400

    lets go this works

---
+++ findOddInteger.js
@@ -0,0 +1,17 @@
+const findOdd = array => {
+	return 0;
+};
+
+// Test Cases:
+
+console.log(findOdd(([7], 7)));
+console.log(findOdd(([0], 0)));
+console.log(findOdd(([1, 1, 2], 2)));
+console.log(findOdd(([0, 1, 0, 1, 0], 0)));
+console.log(findOdd(([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1], 4)));
+console.log(findOdd(([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5], 5)));
+console.log(findOdd(([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5], -1)));
+console.log(findOdd(([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5], 5)));
+console.log(findOdd(([10], 10)));
+console.log(findOdd(([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1], 10)));
+console.log(findOdd(([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10], 1)));
